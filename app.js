c5929a717b809738c7ccb3c1dea509bf4eb7f9e3
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-08 14:32:15 +0100

    CORS working

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const app = express();
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+app.use(cors());
 
 const port = process.env.PORT || 3000
 
