2e6251d663d6321055bf49fcc6fe4b1db69d3354
Steffen Häfelinger <s.haefelinger@gmx.de>
2022-01-13 18:01:49 +0100

    public

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public/blogpostPics'));
 
 // const imageUpload = require(__basedir + '/services/imageUpload');
 // app.use(imageUpload);
